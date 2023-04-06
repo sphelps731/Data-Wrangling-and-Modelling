f886e6b84972f3561d58504905d9d1a30f0e6ba7
sjohnp2112 <99998544+sjohnp2112@users.noreply.github.com>
2023-04-06 15:10:52 -0400

    Add files via upload

--- sql_queries_orig.py
+++ sql_queries_orig.py
@@ -12,20 +12,25 @@ songplay_table_create = ("CREATE TABLE songplays (songplay_id SERIAL PRIMARY KEY
                          start_time timestamp NOT NULL, user_id varchar NOT NULL, \
                          level varchar, song_id varchar NOT NULL, \
                          artist_id varchar NOT NULL, session_id varchar NOT NULL, location \
-                         varchar, user_agent varchar)")
+                         varchar, user_agent varchar, UNIQUE(user_id), \
+                         UNIQUE(song_id), UNIQUE(artist_id))")
                          
 
 user_table_create = ("CREATE TABLE users (user_id varchar PRIMARY KEY, \
                      first_name varchar NOT NULL, last_name varchar NOT NULL, \
-                     level varchar)")
+                     level varchar, FOREIGN KEY (user_id) REFERENCES songplays (user_id))")
 
 song_table_create = ("CREATE TABLE songs (song_id varchar PRIMARY KEY, title text, \
                      artist_id varchar NOT NULL, year int, \
-                     duration int NOT NULL)")
+                     duration int NOT NULL,\
+                     UNIQUE(artist_id),\
+                     FOREIGN KEY(song_id) REFERENCES songplays(song_id))")
 #                    
 
 artist_table_create = ("CREATE TABLE artists (artist_id varchar PRIMARY KEY, name varchar, \
-                       location varchar, latitude DECIMAL(8,6), longitude DECIMAL(9,6))")
+                       location varchar, latitude DECIMAL(8,6), longitude DECIMAL(9,6), \
+                       FOREIGN KEY (artist_id) REFERENCES songplays(artist_id),\
+                       FOREIGN KEY (artist_id) REFERENCES songs(artist_id))")
 
 time_table_create = ("CREATE TABLE time (start_time timestamp PRIMARY KEY, hour int, day int, \
                     week int, month int,  year int, weekday int)")
@@ -41,8 +46,8 @@ user_table_insert = ("""
 song_table_insert = ("""INSERT INTO songs(song_id, title, artist_id, year, duration)\
                         VALUES(%s, %s, %s, %s, %s)""")
 
-artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude)\
-                        VALUES(%s, %s, %s, %s, %s)""")
+artist_table_insert = ("""
+""")
 
 
 time_table_insert = ("""
